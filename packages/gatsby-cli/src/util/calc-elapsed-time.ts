3e9cdc65faf4b18a25bf959ee97b56d3b5504ed0
Adam Schay <adamschay@gmail.com>
2022-10-14 21:58:42 -0700

    chore: esm updates

--- packages/gatsby-cli/src/util/calc-elapsed-time.ts
+++ packages/gatsby-cli/src/util/calc-elapsed-time.ts
@@ -1,7 +1,7 @@
 import convertHrtime from "convert-hrtime"
 
-export function calcElapsedTime(startTime: [number, number]): string {
-  const elapsed = process.hrtime(startTime)
+export function calcElapsedTime(startTime: bigint): string {
+  const elapsed = process.hrtime.bigint() - startTime
 
   return convertHrtime(elapsed)[`seconds`].toFixed(3)
 }
